69511fee6127b62b91ba281e4c3ed645434ea18e
Jean Santos <jeansantos.sw@gmail.com>
2021-07-24 10:41:18 +0100

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Ignews</title>
+      </Head>
+      
+      <h1>Hello World</h1>
+    </>
+  )
+}

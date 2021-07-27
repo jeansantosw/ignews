7b5e3737f998231b028d4bfb01e9550d7a417fab
Jean Santos <jeansantos.sw@gmail.com>
2021-07-27 19:49:14 +0100

    up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,20 @@ export default function Home() {
       <Head>
         <title>Ignews</title>
       </Head>
-      
-      <h1>Hello World</h1>
+      <main>
+        <section>
+          <span>👏 Hey, welcome </span>
+          <h1>News about the <span>React</span> world. </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+
+        </section>
+
+        <img src="/images/avatar.svg" alt="Gril coding" />
+        
+      </main>
     </>
   )
 }

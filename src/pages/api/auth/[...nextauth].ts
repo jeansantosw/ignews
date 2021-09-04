5b08fe598cb71efb5167ea281c7e8cfe1c332710
Jean Santos <jeansantos.sw@gmail.com>
2021-09-04 12:40:45 +0100

    verificando usuario criado no bando

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,7 +3,7 @@ import { query as q } from 'faunadb'
 import NextAuth from "next-auth"
 import Providers from "next-auth/providers"
 
-import { fauna } from '../../../services/fauna'
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -14,17 +14,46 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
+  
   callbacks: {
+
     async signIn(user, account, profile) {
-      
+      const { email } = user;
+
+      try {
         await fauna.query(
-          q.Create(
-            q.Collection('users'),
-            {data: user }
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
           )
         )
-      
-      return true;
+
+
+
+
+        return true;
+      } catch {
+        return false;
+      }
+
     }
   }
 })
+
+

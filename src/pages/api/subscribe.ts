fadb731193a919db43271aba7d047e785192aaff
Jean Santos <jeansantos.sw@gmail.com>
2021-09-07 12:38:35 +0100

    corrigindo cliente duplicado;

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,8 +1,19 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { query as q } from 'faunadb';
 
 import { getSession } from 'next-auth/client';
+import { fauna } from "../../services/fauna";
 
 import { stripe } from '../../services/stripe';
+
+interface User {
+  ref: {
+    id: string;
+  }
+  data: {
+    stripe_customer_id: string;
+  }
+}
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -10,13 +21,38 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST'){
     const session = await getSession({ req });
 
-    const stripeCustomer = await stripe.customers.create({
-      email: session.user.email,
-    })
+    const user = await fauna.query<User>(
+      q.Get(
+        q.Match(
+          q.Index('user_by_email'),
+          q.Casefold(session.user.email)
+        )
+      )
+    )
+
+    let customerId = user.data.stripe_customer_id;
+
+    if(!customerId){
+      const stripeCustomer = await stripe.customers.create({
+        email: session.user.email,
+      })
+
+      await fauna.query(
+        q.Update(
+          q.Ref(q.Collection('users'), user.ref.id),
+          {
+            data: {
+              stripe_customer_id: stripeCustomer.id,
+            }
+          }
+        )
+      )
+      customerId = stripeCustomer.id
+    }
 
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
 
-      customer: stripeCustomer.id,
+      customer: customerId,
       payment_method_types: ['card'],
       billing_address_collection: 'required',
       line_items: [ {
@@ -28,9 +64,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       cancel_url: process.env.STRIPE_CANCEL_URL
 
     })
-
-   
-
 
     return res.status(200).json({ sessionId: stripeCheckoutSession.id });
 
